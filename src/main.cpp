e4328dbd14f8f96b3ed52e3b56b4ebffa37c0330
leonhardt <lariscusObscurus@gmail.com>
2018-03-27 21:53:16 +0200

    Added TBB

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include "Image/Image.h"
+#include <tbb/parallel_for.h>
 
 
 int main(int argc, char*argv[])
 {
+    tbb::parallel_for(tbb::blocked_range<size_t>(0, 10), [](const tbb::blocked_range<size_t>& i)
+    {
+        std::cout << "hello world" << std::endl;
+    });
+
 	auto test_image = image_io("C:/Users/Leonhardt/Pictures/t.PNG");
 
     auto image =test_image.get_image();
