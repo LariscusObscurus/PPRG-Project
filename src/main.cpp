7ebecee22b553cf0e68569498d30b673bfdd21b5
Peter Hoffmann <peter@p-h.im>
2018-04-08 12:48:25 +0200

    compare elapsed times of sequential/parallel impl

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,54 @@
 #include "Convolution/Convolution.h"
 #include "Combine/Combine.h"
 #include <chrono>
+#include <vector>
+#include <algorithm>
+#include <numeric>
 
+std::chrono::duration<double, std::milli> run(image_matrix<float> image,
+                                              image_io &image_io,
+                                              image_matrix<float> sobel_h,
+                                              image_matrix<float> sobel_v,
+                                              image_matrix<float>& (convolution<float>::*apply_kernel)(
+                                                                                 image_matrix<float>&, image_matrix<float>&, image_matrix<float>&),
+                                              image_matrix<float>& (combine<float>::*combine_matrices)(
+                                                                                 image_matrix<float>&, image_matrix<float>&, image_matrix<float>&))
+{
+    convolution<float> convolution;
+    combine<float> combine;
+    auto result_h = image_matrix<float>(image.rows(), image.cols());
+    auto result_v = image_matrix<float>(image.rows(), image.cols());
+    auto combined = image_matrix<float>(image.rows(), image.cols());
+
+    const auto start_time = std::chrono::steady_clock::now();
+    (convolution.*apply_kernel)(result_h, image, sobel_h);
+    (convolution.*apply_kernel)(result_v, image, sobel_v);
+
+    (combine.*combine_matrices)(combined, result_h, result_v);
+
+    const auto end_time = std::chrono::steady_clock::now();
+    const auto diff = end_time - start_time;
+
+//    image_io.write("./out.png", combined.cast<unsigned char>());
+
+    return diff;
+}
+
+void print_stats(std::vector<double> times)
+{
+    auto minmax = std::minmax_element(times.begin(), times.end());
+    auto avg = std::accumulate(times.begin(), times.end(), 0.0) / times.size();
+
+    std::cout
+    << "Min: " << times.at(minmax.first - times.begin()) << ", "
+    << "Max: " << times.at(minmax.second - times.begin()) << ", "
+    << "Average: " << avg
+    << std::endl;
+}
 
 int main(int argc, char*argv[])
 {
-	auto test_image = image_io("C:/Users/Leonhardt/Pictures/Lenna.png");
+    auto test_image = image_io("./t.png");
     image_matrix<float> image = test_image.get_image().cast<float>();
 
     image_matrix<float> sobel_h(3, 3);
@@ -21,27 +64,32 @@ int main(int argc, char*argv[])
             2, 0, -2,
             1, 0, -1;
 
-    convolution<float> convolution;
-    auto result_h = image_matrix<float>(image.rows(), image.cols());
-    auto result_v = image_matrix<float>(image.rows(), image.cols());
-    auto combined = image_matrix<float>(image.rows(), image.cols());
+    std::cout << "Running sequentially" << std::endl;
+    std::vector<double> times_seq;
+    for (int i = 0; i < 10; i++)
+    {
+        auto diff = run(image, test_image, sobel_h, sobel_v,
+                        &convolution<float>::apply_kernel_sequentially,
+                        &combine<float>::combine_matrices_sequentially);
+        double count = std::chrono::duration<double, std::milli>(diff).count();
+        times_seq.push_back(count);
+        std::cout << count << " ms" << std::endl;
+    }
+    print_stats(times_seq);
+    std::cout << std::endl;
 
-    //convolution.apply_kernel_sequentially(result, image, sobel_h);
-    const auto start_time = std::chrono::steady_clock::now();
-    convolution.apply_kernel_parallel(result_h, image, sobel_h);
-    convolution.apply_kernel_parallel(result_v, image, sobel_v);
-
-    combine<float> combine;
-    combine.combine_matrices_parallel(combined, result_h, result_v);
-
-    const auto end_time = std::chrono::steady_clock::now();
-    const auto diff = end_time - start_time;
-    std::cout << std::chrono::duration<double, std::milli>(diff).count() << " ms" << std::endl;
-
-    
-
-
-    test_image.write("C:/Users/Leonhardt/Pictures/out.png", combined.cast<unsigned char>());
+    std::cout << "Running parallel" << std::endl;
+    std::vector<double> times_par;
+    for (int i = 0; i < 10; i++)
+    {
+        auto diff = run(image, test_image, sobel_h, sobel_v,
+                   &convolution<float>::apply_kernel_parallel,
+                   &combine<float>::combine_matrices_parallel);
+        double count = std::chrono::duration<double, std::milli>(diff).count();
+        times_par.push_back(count);
+        std::cout << count << " ms" << std::endl;
+    }
+    print_stats(times_par);
 
     std::getchar();
     return 0;
